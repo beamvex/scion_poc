a8b1298c245357cf453002ac7e1b1d1988766e1b
Robert Forster <robert.forster@beamvex.co.uk>
2026-04-04 13:38:39 +0100

    refactor: remove beacon and dataplane services, simplify to minimal HTTP-only router
    - Remove beacon service (beacon.rs, beacon_listen/beacon_interval_secs CLI args, BeaconMsg/BeaconPeer types)
    - Remove dataplane packet forwarding (dataplane.rs, data_listen CLI arg, run_dataplane task)
    - Remove SCION packet parsing module (scion.rs, parse_scion_dst_ia/parse_scion_fwd_meta)
    - Remove routing table and interface management (routing.rs types, HTTP routes/ifaces/beacon endpoints)
    - Remove metrics tracking

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,32 @@
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 
 use clap::Parser;
-use tokio::sync::RwLock;
 use tokio::task::JoinSet;
-use tracing::{error, info};
 
-mod beacon;
 mod cli;
-mod dataplane;
 mod http_api;
-mod scion;
 mod state;
-
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
-        )
-        .init();
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    init_tracing();
+
+    slogger::info!("Starting SCION router prototype");
 
     let cli = cli::Cli::parse();
 
-    let state = state::AppState {
-        rt: Arc::new(RwLock::new(state::RoutingTable::default())),
-        ifaces: Arc::new(RwLock::new(HashMap::new())),
-        iface_socks: Arc::new(RwLock::new(HashMap::new())),
-        beacon_peers: Arc::new(RwLock::new(Vec::new())),
-        metrics: Arc::new(state::Metrics::default()),
-    };
-
+    let state = Arc::new(state::AppState {});
+    
     let mut set = JoinSet::<anyhow::Result<()>>::new();
-    set.spawn(http_api::run_http(state.clone(), cli.http_listen));
-    set.spawn(dataplane::run_dataplane(state.clone(), cli.data_listen));
-    set.spawn(beacon::run_beacon_rx(state.clone(), cli.beacon_listen));
-    set.spawn(beacon::run_beacon_tx(state.clone(), cli.beacon_interval_secs));
-
+    set.spawn(http_api::run_http(Arc::clone(&state), cli.http_listen));
+    
     while let Some(res) = set.join_next().await {
         match res {
             Ok(Ok(())) => {
-                info!("task exited");
+                slogger::info!("task exited");
                 break;
             }
             Ok(Err(e)) => {
-                error!(error=%e, "task failed");
+                slogger::error!("task failed: {e}");
                 return Err(e);
             }
             Err(e) => {
